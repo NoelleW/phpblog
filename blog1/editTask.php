9474ec45a2a8c93b8dcc391e604a6d3fbf0b4c83
Noelle <Noelle.wachter@student.rocleiden.nl>
2018-06-23 16:17:27 +0200

    updates working

--- blog1/editTask.php
+++ blog1/editTask.php
@@ -1,10 +1,21 @@
+<a href="index.php">Ga naar het overzicht</a>
+<form action="editTask.php" method="post" >
+    <p>
+        <label for="taskTitle">Taak:</label>
+        <input type="text" name="title" id="title" required>
+    </p>
+    <p>
+        <label for="Content">Beschrijving: </label>
+        <input type="text" name="content" id="content">
+    </p>
+    <input type="submit" value="Submit">
+</form>
+
+
 <?php
 require_once("query.php");
+$title = $_POST["title"];
+$content = $_POST["content"];
+$id = $_REQUEST['id'];
+$sql = "UPDATE tasks SET title='$title', content='$content' WHERE id='$id'";
 
-$sql = "UPDATE tasks SET title=$title WHERE id=$id";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
-} else {
-    echo "Error updating record: " . $conn->error;
-}
--- blog1/index.php
+++ blog1/index.php
@@ -12,7 +12,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "Taak nr.: ". $row["id"] . "<br>" . "Taak: " . $row["title"] . "<br>" . "Beschrijving: " .$row["content"]. "<br>". $row["date"].
-            "<a href=\"deleteTask.php?id=$row[id]\">Delete</a><br><br>" ;
+            "<a href=\"editTask.php?id=$row[id]\">Update</a> <a href=\"deleteTask.php?id=$row[id]\">Delete</a><br><br>" ;
     }
 }
 
